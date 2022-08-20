0c55841bbbfe558cacf5cddd661d68b595b13bac
cykim8811 <cykim8811@snu.ac.kr>
2022-08-20 11:23:48 +0000

    Change from A2C to A3C

--- a3c.py
+++ a3c.py
@@ -7,6 +7,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optimizer
 
+def ensure_shared_grads(model, shared_model):
+    for param, shared_param in zip(model.parameters(), shared_model.parameters()):
+        if shared_param.grad is not None:
+            return
+        shared_param._grad = param.grad
 
 class Model(nn.Module):
     def __init__(self, action_count):
@@ -29,48 +34,65 @@ class Model(nn.Module):
         x = self.linear(x)
         return self.actor(x), self.critic(x)
 
-model = Model(2).cuda()
+class Environment:
+    def __init__(self):
+        self.env = gym.make('CartPole-v1')
+        self.model = Model(2).cuda()
+        self.obs = self.env.reset()
+        self.log = []
+        self.score_log = [0]
+        
 
-opt = optimizer.Adam(model.parameters(), lr=0.0017)
+global_model = Model(2).cuda()
 
-env = gym.make('CartPole-v1')
-obs = env.reset()
+opt = optimizer.Adam(global_model.parameters(), lr=0.0013, weight_decay=1e-5)
 
-log = []
 
 gamma = 0.99
+max_timesteps = 20
+
+environments = [Environment() for _ in range(8)]
+
 
 while True:
-    a, c = model(torch.tensor(obs).cuda())
-    log_prob = a
-    action = torch.exp(log_prob).multinomial(1).detach().item()
-    s1, reward, done, info = env.step(action)
-    log.append((obs, action, reward, s1, log_prob, c))
-    obs = s1
-    if done: obs = env.reset()
+    for E in environments:
+        a, c = E.model(torch.tensor(E.obs).cuda())
+        log_prob = a
+        action = torch.exp(log_prob).multinomial(1).detach().item()
+        s1, reward, done, info = E.env.step(action)
+        E.log.append((E.obs, action, reward, s1, log_prob, c))
+        E.obs = s1
+        if done: E.obs = E.env.reset()
+        
+        # Score logging
+        E.score_log[-1] += 1
+        if done:
+            print(E.score_log[-1])
+            E.score_log.append(0)
 
-    if done:
-        sum_loss = torch.tensor(0.0).cuda()
-        for i, data in enumerate(log):
-            s0, action, reward, s1, log_prob, c0 = data
 
-            c1 = log[i+1][5].detach()[0] if i < len(log)-1 else torch.tensor(0.0).cuda()
+        if done or len(E.log) >= max_timesteps:
+            sum_loss = torch.tensor(0.0).cuda()
+            td_target = E.model(torch.tensor(s1).cuda())[1] if not done else torch.tensor(0.0).cuda()
+            for i, data in enumerate(reversed(E.log)):
+                s0, action, reward, s1, log_prob, c0 = data
 
-            td_target = torch.tensor(reward).cuda() + gamma * c1
-            td_error = td_target - c0.detach()[0]
+                td_target = E.log[-i][5].detach()[0] if i!=0 else torch.tensor(0.0).cuda()
 
-            value_loss = F.mse_loss(c0, td_target)
-            policy_loss = -log_prob[action] * td_error
+                td_target = torch.tensor(reward).cuda() + gamma * td_target
+                td_error = td_target - c0.detach()[0]
 
-            total_loss = value_loss + policy_loss
-            sum_loss += total_loss
+                value_loss = F.mse_loss(c0, td_target)
+                policy_loss = -log_prob[action] * td_error + 0.01 * torch.sum(log_prob * torch.exp(log_prob))
+
+                total_loss = value_loss + policy_loss
+                sum_loss += total_loss
             
-        opt.zero_grad()
-        sum_loss.backward()
-        opt.step()
-        
-        print(len(log))
-        
-        log = []
-
-    
+            ensure_shared_grads(E.model, global_model)
+            
+            opt.zero_grad()
+            sum_loss.backward()
+            opt.step()
+            
+            E.log = []
+            E.model.load_state_dict(global_model.state_dict())
